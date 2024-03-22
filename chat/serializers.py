6ceda8c9bfaf3b73d1a7597c97a4cd1a276e0211
ODyachenko <oleg1696@gmail.com>
2024-03-22 16:33:44 +0200

    chat serializers

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,17 +1,11 @@
 from .models import User, Room, Message
 from rest_framework import serializers
-from users.serializers import UserInfoSerializer
-
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         exclude = ["password"]
+from users.serializers import CustomUserSerializer
 
 
 class MessageSerializer(serializers.ModelSerializer):
     created_at_formatted = serializers.SerializerMethodField()
-    user = UserInfoSerializer()
+    user = CustomUserSerializer()
 
     class Meta:
         model = Message
@@ -19,17 +13,18 @@ class MessageSerializer(serializers.ModelSerializer):
         depth = 1
 
     def get_created_at_formatted(self, obj:Message):
-        return obj.created_at.strftime("%d-%m-%Y %H:%M:%S")
+        # return obj.created_at.strftime("%d-%m-%Y %H:%M:%S")
+        return obj.created_at.strftime("%H:%M")
 
 class RoomSerializer(serializers.ModelSerializer):
     last_message = serializers.SerializerMethodField()
     messages = MessageSerializer(many=True, read_only=True)
-    host = UserInfoSerializer()
-    current_users = UserInfoSerializer()
+    host = CustomUserSerializer()
+    current_users = CustomUserSerializer()
 
     class Meta:
         model = Room
-        fields = ["pk", "name", "host", "messages", "current_users", "last_message"]
+        fields = ["id", "host", "messages", "current_users", "last_message"]
         depth = 1
         read_only_fields = ["messages", "last_message"]
 
