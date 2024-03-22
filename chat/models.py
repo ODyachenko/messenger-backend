212dbdf866bbf7c9a4bc8acb48c11b8c0af920b2
ODyachenko <oleg1696@gmail.com>
2024-03-22 16:33:29 +0200

    chat models

--- chat/models.py
+++ chat/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-from users.models import Profile
+from users.models import CustomUser
 
 class Room(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
-    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rooms")
-    current_users = models.ManyToManyField(User, related_name="current_rooms", blank=True)
+    host = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="rooms")
+    current_users = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="current_rooms", blank=True, null=True)
 
     def __str__(self):
-        return f"Room({self.name} {self.host})"
+        return f"Room({self.host}) -> {self.current_users}"
 
 
 class Message(models.Model):
-    room = models.ForeignKey("chat.Room", on_delete=models.CASCADE, related_name="messages")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
     text = models.TextField(max_length=500)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="messages")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
